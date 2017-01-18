b025e7d1abd0cd4168c4e903493975c0850c2fec
Harold Putman <harold.putman@lexmark.com>
2017-01-18 14:33:47 -0500

    Testing Class vs. Script.
    Can I write a groovy file that can be used both as a Class (loaded from another groovy script) and as a script (invoked directly by Jenkins).

---
+++ jenkins/Hello.groovy
@@ -0,0 +1,16 @@
+import org.codehaus.groovy.runtime.InvokerHelper
+class Hello extends Script {
+    def run2() {
+        println 'Goodbye world!'
+    }
+
+    def run() {
+        println 'Hello world!'
+        return this
+    }
+
+    static void main(String[] args) {
+        println 'In Main';
+        InvokerHelper.runScript(Main, args)
+    }
+}
