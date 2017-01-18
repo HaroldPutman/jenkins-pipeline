4e80d1544869274e5e7bbf83fbc4209601d4f322
Harold Putman <harold.putman@lexmark.com>
2017-01-18 16:28:39 -0500

    pwd is full path

--- jenkins/HelloInvoke.groovy
+++ jenkins/HelloInvoke.groovy
@@ -1,6 +1,6 @@
 def Hello
 node ('master') {
     def workspace = pwd()
-    Hello = load "../${workspace}@script/jenkins/hello.groovy"
+    Hello = load "${workspace}@script/jenkins/hello.groovy"
 }
 Hello.say()
