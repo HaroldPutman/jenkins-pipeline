7ea1ab6a8172a2ddb4312c0cc92ccfa54f787950
Harold Putman <harold.putman@lexmark.com>
2017-06-24 10:25:18 -0700

    doh. How did I not see this?

--- jenkins/mainer.groovy
+++ jenkins/mainer.groovy
@@ -1,17 +1,5 @@
 
 def start() {
-  stage('init') {
-    node('linux') {
-      unstash 'all'
-      sh 'ls -lR'
-      sh 'cat jenkins/resource.txt'
-    }
-  }
-  stage('checkout') {
-    node('linux') {
-      checkout scm
-    }
-  }
   stage('main') {
     node('linux') {
       unstash 'all'
